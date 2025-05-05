dc9a38ab1c78dd88d05f1e0a7c4d5b0961d06f05
ahsansheikh14 <146324815+ahsansheikh14@users.noreply.github.com>
2025-05-05 18:56:30 +0500

    Update DBConnection password and ensure MySQL dependency is present

--- IdeaProjects/demo/src/main/java/com/example/carrental/DBConnection.java
+++ IdeaProjects/demo/src/main/java/com/example/carrental/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/carrental";
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "ahsan1234";
 
     public static Connection getConnection() throws SQLException {
         try {
